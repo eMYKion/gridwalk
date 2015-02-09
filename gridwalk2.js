1bc8e60d8537f0ef923940b878a38c7833a0a627
eMYKion <eMYKion@users.noreply.github.com>
2015-02-09 11:14:21 -0500

    re-launched as resource for Willy-VVu's SmashLab

--- gridwalk2.js
+++ gridwalk2.js
@@ -8,13 +8,15 @@ dirq = new Array(4);
 var t;
 
 //objects
+
 var building, plant, plane, texture;
 
 
 
 //
+
 const PI=Math.PI;
-const camspd=100;
+const camspd=100*100;
 const turnspd=PI/32;
 var rotY=0;
 
@@ -34,44 +36,51 @@ function init() {
     
     
     //create the camera object
-    camera = new THREE.PerspectiveCamera( 75, windowX / windowY, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 75, windowX / windowY, 1, 1000000 );
     camera.position.set(0,5,1000);
-    
+    camera.eulerOrder="YXZ";//precedence for changing axis, to  ensure proper rotation
     //include a camera: object-oriented
     controls = new THREE.OrbitControls( camera );
     
-    /*
     //tron texture for THE GRID    TEXTURE MUST BE A POWER OF TWO
+    
     texture = new THREE.ImageUtils.loadTexture("TRON_Tile.png");
     texture.wrapS=texture.wrapT=THREE.RepeatWrapping;
-    texture.repeat.set(10000,10000);
+    texture.repeat.set(2500,2500);
     texture.anisotropy=renderer.getMaxAnisotropy();
-    */
     
     
-    /*
+    
+    
     //random blender stuff
     
     loader = new THREE.JSONLoader();
     
     
     function lld( geometry ) {
-        map = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
-        map.scale.set( 1000, 1000, 1000 );
-        map.position.set(0,0,0);
-        scene.add(map);
+        nexus = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+        nexus.scale.set( 1000, 1000, 1000 );
+        nexus.position.set(0,1000,-1000);
+        scene.add(nexus);
+        
+    }
+    
+    function lld2( geometry ) {
+        monkey = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+        monkey.scale.set( 1000, 1000, 1000 );
+        monkey.position.set(0,1000,0);
+        scene.add(monkey);
         
     }
     
     
-    loader.load( "map.js", lld);
+    loader.load( "nexus.js", lld);
+    //loader.load( "test.js", lld2);
     
     
     
     
-    */
     //create THE GRID
-    /*
     plane = new THREE.Mesh( new THREE.PlaneGeometry(1000000, 1000000), new THREE.MeshBasicMaterial(
         
         {
@@ -83,16 +92,16 @@ function init() {
     plane.position.set(0, 0, 0);
     //put THE GRID in the xz-plane\/
     plane.rotation.x=-PI/2;
-    */
-    /*
-    building = new THREE.Mesh(new THREE.BoxGeometry(50,1000,50), new THREE.MeshBasicMaterial(
+    
+    
+    building = new THREE.Mesh(new THREE.BoxGeometry(50,10000,50), new THREE.MeshBasicMaterial(
         {
             color: 0x00ddff,
-            wireframe: true
+            wireframe: false
         }
     ));
     
-    building.position.y+=500+1;
+    building.position.set(0,10000/2+2500,-1000);
     
     
     plant = new THREE.Mesh(new THREE.BoxGeometry(50,100,50), new THREE.MeshBasicMaterial(
@@ -102,19 +111,19 @@ function init() {
         }
     ));
     
-    plant.position.set(100,50+1,0);
+    plant.position.set(100,50+5,0);
     
     
     //add objects to scene
     scene = new THREE.Scene();
-    scene.add(camera, plant, building);
+    scene.add(camera, plane, plant, building);
     
     
     
     //add event listeners
     document.addEventListener('keydown', movecamtr, false);
     document.addEventListener('keyup', movecamfa, false);
-    */
+    
     /*KEYCODES
     w - 87   moveq[0]
     a - 65   moveq[2]
@@ -126,7 +135,7 @@ function init() {
     r - 82   dirq[2]
     f - 70   dirq[3]
     */
-    /*
+    
     function movecamtr(event){
         
         if(event.keyCode==87){//if w pressed
@@ -145,7 +154,7 @@ function init() {
         if(event.keyCode==81){//if q pressed
             dirq[0]=true;
         }
-        if(event.keyCode==69){//if q pressed
+        if(event.keyCode==69){//if e pressed
             dirq[1]=true;
         }
         if(event.keyCode==82){//if r pressed
@@ -188,7 +197,7 @@ function init() {
     }
     
     
-    */
+    
     //end of init()
 }
 
@@ -205,13 +214,17 @@ function animate() {
         curr = new Date();
     }
     t+=10/1000;
+    
     requestAnimationFrame( animate );
     render();
 }
 
-
+var tempVec;
 function render() {
-    
+  
+    if(!tempVec){
+         tempVec= new THREE.Vector3(0,0,0);
+    }
     
     //
     
@@ -247,29 +260,39 @@ function render() {
     
     if(dirq[2]==true){//r key
         camera.position.y+=Math.sqrt(camspd);
-        
     }
     if(dirq[3]==true){//f key
         camera.position.y-=Math.sqrt(camspd);
         
     }
     
-    
+    tempVec.set(0,0,0);
     if(moveq[0]==true){//w key
-        camera.position.z-=Math.sqrt(camspd)*(Math.cos(rotY));
-        camera.position.x+=Math.sqrt(camspd)*(Math.sin(rotY));
+        tempVec.z-=1;
+//        camera.position.z-=Math.sqrt(camspd)*(Math.cos(rotY));
+//        camera.position.x+=Math.sqrt(camspd)*(Math.sin(rotY));
+        
     }
     if(moveq[1]==true){//s key
-        camera.position.z+=Math.sqrt(camspd)*(Math.cos(rotY));
-        camera.position.x-=Math.sqrt(camspd)*(Math.sin(rotY));
+        tempVec.z+=1;
+//        camera.position.z+=Math.sqrt(camspd)*(Math.cos(rotY));
+//        camera.position.x-=Math.sqrt(camspd)*(Math.sin(rotY));
     }
     if(moveq[2]==true){//a key
-        camera.position.x-=Math.sqrt(camspd)*(Math.sin(rotY+PI/2));
-        camera.position.z+=Math.sqrt(camspd)*(Math.cos(rotY+PI/2));
+        tempVec.x-=1;
+//        camera.position.x-=Math.sqrt(camspd)*(Math.sin(rotY+PI/2));
+//        camera.position.z+=Math.sqrt(camspd)*(Math.cos(rotY+PI/2));
     }
     if(moveq[3]==true){//d key
-        camera.position.x+=Math.sqrt(camspd)*(Math.sin(rotY+PI/2));
-        camera.position.z-=Math.sqrt(camspd)*(Math.cos(rotY+PI/2));
+        tempVec.x+=1;
+//        camera.position.x+=Math.sqrt(camspd)*(Math.sin(rotY+PI/2));
+//        camera.position.z-=Math.sqrt(camspd)*(Math.cos(rotY+PI/2));
+    }
+    if(tempVec.length()>0){
+        tempVec.normalize();
+        tempVec.multiplyScalar(Math.sqrt(camspd));
+        tempVec.applyQuaternion(camera.quaternion);
+        camera.position.add(tempVec);
     }
     /*
     if(moveq[0]==true){//w key
@@ -285,6 +308,8 @@ function render() {
         camera.position.x+=camspd;
     }
     */
+    
+    //camera.rotation.x+=0.01;
     renderer.render( scene, camera ); 
     
     
